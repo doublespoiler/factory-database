71f8dadd67cac602fc6a821c648926f969299c2f
Skylan Lew <skylanslew@gmail.com>
2022-10-14 12:01:32 -0700

    initial database migration

---
+++ Factory/Migrations/20221014184450_Initial.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Machines",
+                columns: table => new
+                {
+                    MachineId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    MachineName = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    AssetNumber = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    LocationNumber = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Machines", x => x.MachineId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mechanics",
+                columns: table => new
+                {
+                    MechanicId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    MechanicName = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    MechanicTitle = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mechanics", x => x.MechanicId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MechanicMachine",
+                columns: table => new
+                {
+                    MechanicMachineId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    MechanicId = table.Column<int>(type: "int", nullable: false),
+                    MachineId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MechanicMachine", x => x.MechanicMachineId);
+                    table.ForeignKey(
+                        name: "FK_MechanicMachine_Machines_MachineId",
+                        column: x => x.MachineId,
+                        principalTable: "Machines",
+                        principalColumn: "MachineId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MechanicMachine_Mechanics_MechanicId",
+                        column: x => x.MechanicId,
+                        principalTable: "Mechanics",
+                        principalColumn: "MechanicId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MechanicMachine_MachineId",
+                table: "MechanicMachine",
+                column: "MachineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MechanicMachine_MechanicId",
+                table: "MechanicMachine",
+                column: "MechanicId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MechanicMachine");
+
+            migrationBuilder.DropTable(
+                name: "Machines");
+
+            migrationBuilder.DropTable(
+                name: "Mechanics");
+        }
+    }
+}
